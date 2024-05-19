5109948433ba452295ccb9fa2df636f33ca15b49
qingzhuyue <qingzhuyue@foxmail.com>
2024-05-19 11:37:04 +0800

    fix:3d太空

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
  * @Author: qingzhuyue qingzhuyue@foxmail.com
  * @Date: 2024-05-03 22:23:28
  * @LastEditors: qingzhuyue qingzhuyue@foxmail.com
- * @LastEditTime: 2024-05-10 18:14:22
+ * @LastEditTime: 2024-05-18 20:22:51
  * @FilePath: /react-three/src/App.jsx
  * @Description: 
  * Copyright (c) 2024 by ${qingzhuyue} email: ${qingzhuyue@foxmail.com}, All Rights Reserved.
@@ -13,13 +13,14 @@ import MeshPhongMaterial from './components/MeshPhongMaterial'
 import MeshNormalMaterial from "./components/MeshNormalMaterial"
 import AmbientLight from './components/AmbientLight'
 import PointLight from './components/PointLight'
+import OuterSpace from './views/OuterSpace'
 
 function App() {
 
   return (
     <>
       <div>
-       <PointLight />
+       <OuterSpace />
       </div>
     </>
   )
